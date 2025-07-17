f6db95f97d9ee7c1d42ae6e940627ce760b64e6c
phat2399 <22dh112698@st.huflit.edu.vn>
2025-07-18 03:16:13 +0700

    demolab8neww

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def home():
     </head>
     <body>
         <div class="container">
-            <h1>🚀 CI/CD Pipeline Đã Tự Động Cập Nhật Nhé !!!</h1>
+            <h1>🚀 DEMO MỞ RỘNG Tích hợp Kubernetes để triển khai trên AWS EKS Thành công nha thầy!!!</h1>
             <p>Trang này đã được triển khai tự động bởi Jenkins.</p>
         </div>
     </body>
